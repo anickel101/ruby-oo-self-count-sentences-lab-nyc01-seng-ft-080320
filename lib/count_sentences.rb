bb92917215100cd0148aeb20533dc98a2724a0b6
anickel101 <anson.nickel@gmail.com>
2020-08-04 16:10:37 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    array = self.chars.slice_before do |x|
+      x == "!" || x == "?" || x == "."
+    end.to_a
+    array.delete_if {|sent| sent.length < 2}.count
   end
+
 end
+
+test = "This is my test sentence. It's great! It's fine. No, it's great!!!" 
+# => test.count_sentences should == 4
+
+#binding.pry
+
+
